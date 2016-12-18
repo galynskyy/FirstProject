b8fd6be85840e94c3d10f921476ec2c29b2624f1
Vladimir Galynsky <galynskyy@gmail.com>
2016-12-18 22:49:03 +0700

    add dev gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
+var browserSync = require("browser-sync").create();
 
-gulp.task("css", function(){
+gulp.task("dev", ["styles", "server", "watch"]);
+
+gulp.task("styles", function(){
 	return gulp.src("./src/styles/*.css")
 		.pipe(concat("build.css"))
 		.pipe(gulp.dest("./src/styles/"));
+});
+
+gulp.task("server", function () {
+	browserSync.init({
+        	server: {
+            		baseDir: "./src"
+        	}
+	});
+});
+
+gulp.task("watch", function(){
+	gulp.watch("./src/styles/**/*.css", ["styles"]);
 });
