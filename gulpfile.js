72be2db16a5b80dee3fb326b35e5273c0aee15d3
Vladimir Galynsky <galynskyy@gmail.com>
2016-12-20 21:59:38 +0700

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var concat = require("gulp-concat");
 var browserSync = require("browser-sync").create();
 
-gulp.task("dev", ["styles", "server", "watch"]);
+gulp.task("dev", ["styles", "html", "js", "assets", "fonts", "server", "watch"]);
 
 gulp.task("styles", function() {
 	return gulp.src("./src/styles/**/*.css")
@@ -10,10 +10,32 @@ gulp.task("styles", function() {
 		.pipe(gulp.dest("./public/styles/"));
 });
 
+gulp.task("html", function() {
+	return gulp.src("./src/*.html")
+		.pipe(gulp.dest("./public/"));
+});
+
+
+gulp.task("assets", () => {
+    gulp.src("./src/assets/**/*.svg")
+        .pipe(gulp.dest("./public/assets/"));
+});
+
+gulp.task("fonts", () => {
+    gulp.src("./src/fonts/**/*.ttf")
+        .pipe(gulp.dest("./public/fonts/"));
+});
+
+gulp.task("js", function() {
+	return gulp.src("./src/scripts/**/*.js")
+		.pipe(concat("build.js"))
+		.pipe(gulp.dest("./public/scripts/"));
+});
+
 gulp.task("server", function () {
 	browserSync.init({
         	server: {
-            		baseDir: "./src"
+            		baseDir: "./public"
         	}
 	});
 });
