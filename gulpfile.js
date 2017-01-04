ac464379e688c06b910b40d05003ef179b9b2b10
andrewshevtsov <andrewshevtsov@inbox.ru>
2017-01-04 18:08:17 +0300

    fix gulpfile, add public folder to .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,59 @@
-var gulp 			= require('gulp'),						// Подключение gulp
-	browserSync 	= require('browser-sync'),				// Подключение browser-sync
-	concat			= require('gulp-concat'),				// Подключение gulp-concat (для конкатенации файлов)
-	cssnano			= require('cssnano'),					// Подключение пакета минификации css
-	rename			= require('gulp-rename'),				// Подключение плагина переименования и добавления суффиксов в файлы
-	del				= require('del'),						// Подключение библиотеки удаления файлов и папок
-	autoprefixer    = require('autoprefixer');				// Подключение библиотеки автодобавления префиксов
-	postcss 		= require("gulp-postcss");				// Подключение postCSS, для тех кто изучил
+var gulp 			= require('gulp');
+var browserSync 	= require('browser-sync');
+var concat			= require('gulp-concat');
+var cssnano			= require('cssnano');
+var rename			= require('gulp-rename');
+var del				= require('del');
+var autoprefixer    = require('autoprefixer');
+var postcss 		= require("gulp-postcss");
 
-gulp.task('styles', function () { 							// Создаем таск styles
-	var processors = [ 										// Создаем переменную
-		autoprefixer({browsers: ['last 1 version']}), 		// Помещаем плагины в переменную
-		cssnano(), 
-	]; 
-	return gulp.src('src/styles/**/*.css') 					// Берем источник
-		.pipe(postcss(processors)) 							// Магия
-		.pipe(gulp.dest('src/styles')); 					// Выгружаем в src/styles
+gulp.task('styles', function () {
+	var processors = [
+		autoprefixer({
+			browsers: ['last 1 version']
+		}),
+		cssnano(),
+	];
+	return gulp.src('src/styles/**/*.css')
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('src/styles'));
 });
 
-gulp.task('browser-sync', function(){						// Создаем таск browser-sync 
-	browserSync({											// Выполняем browser-sync
-		server: {											// Определяем параметры сервера
-			baseDir: 'public'									// Определяем папку сервера
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: {
+			baseDir: 'public'
 		},
-		notify: false										// Отключение уведомлений об успешном коннекте
+		notify: false
 	});
 });
 
-gulp.task('clean', function() {								// Создаем таск clean, 
-	return del.sync('public');								// который чистит папку public перед сборкой
+gulp.task('clean', function() { 
+	return del.sync('public');
 });
 
-gulp.task('watch', ['browser-sync', 'styles'], function(){	
- /* Создаем таск watch, внутри приоритетно запускаем таски browser-sync и css-libs
- 	 чтобы задать последовательность и применились изменения, если таковые были. 
- 	 Необходимо для корректного отображения изменения на момент запуска сервера */ 	 
-	gulp.watch('src/styles/**/*.css', ['styles']);				// Наблюдение за css-файлами в папке styles
-	gulp.watch('src/*.html', browserSync.reload);				// Наблюдение за html-файлами в корне проекта
-	gulp.watch('src/js/**/*.js', browserSync.reload);			// Наблюдение за js-файлами в папке js
+gulp.task('watch', ['browser-sync', 'styles'], function(){ 
+	gulp.watch('src/styles/**/*.css', ['styles']);
+	gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch('src/js/**/*.js', browserSync.reload);
 });
 
-gulp.task('build', ['clean'], function() {						// Создаем таск build, предварительно запустив очистку папки public 
+gulp.task('build', ['clean'], function() {
 	var buildCss = gulp.src([
-			'src/styles/build.min.css',							// Берем из источника минифицированные файлы стилей
+			'src/styles/build.min.css'
 		])
-	.pipe(gulp.dest('public/styles'));							// Выгружаем в public/styles
+	.pipe(gulp.dest('public/styles'));
 
-	var buildFonts = gulp.src('src/fonts/**/*')					// Собираем все шрифты в папке fonts
-	.pipe(gulp.dest('public/fonts'));							// Выгружаем в public/fonts
+	var buildFonts = gulp.src('src/fonts/**/*')
+	.pipe(gulp.dest('public/fonts'));
 
-	var buildJs = gulp.src('src/js/**/*')						// Собираем js-файлы
+	var buildJs = gulp.src('src/js/**/*')
 	.pipe(concat("build.js"))
-	.pipe(gulp.dest('public/js'));								// Выгружаем в public/js
+	.pipe(gulp.dest('public/js'));
 
-	var buildHtml = gulp.src('src/*.html')						// Собираем html-файлы
-	.pipe(gulp.dest('public'));									// Выгружаем в папку public
+	var buildHtml = gulp.src('src/*.html')
+	.pipe(gulp.dest('public'));
 });
+
+gulp.task("dev", ["build", "browser-sync", "watch"]);
+gulp.task("default", ["build"]);
