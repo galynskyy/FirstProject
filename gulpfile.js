c560eab0dc0c331ed510c788fe0654dafa9a4b92
Тимур Чумаков <chumakovtimur@yandex.ru>
2017-01-14 12:38:09 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
-var gulp	  = require('gulp');
+var gulp = require('gulp');
 var minifyCSS = require('gulp-minify-css');
-
-
 gulp.task('mincss', function() {
 	return gulp.src('./static/styles/*.css')
 	.pipe(minifyCSS())
-	.pipe(gulp.dest('./public/styles/default.min.css'))
+	.pipe(gulp.dest('./public/styles/default.min.css'));
 });
-
-
