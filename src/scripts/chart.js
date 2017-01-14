8abab7281cbcac2e5835d27a3207750b8db21d58
Vladimir Galynsky <galynskyy@gmail.com>
2017-01-14 16:13:16 +0700

    add pattern module chart

--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -1,25 +1,35 @@
+var checkboxes = document.querySelectorAll(".goal-checkbox");
+
+var chartModule = (function() {
+    var _init = function() {
+        _eventListeners();
+        _initChart();
+    };
+
+    var _eventListeners = function() {
+        for (var checkbox of checkboxes) {
+            checkbox.addEventListener("click", _initChart);
+        };
+    };
+
+    var _initChart = function() {
+        var circle = document.querySelector(".goal-chart__active");
+        var checkboxesAll = document.querySelectorAll(".goal-checkbox").length;
+        var checkboxesChecked = document.querySelectorAll(".goal-checkbox:checked").length;
+        var percent = document.querySelector(".goal-chart__percent");
+        var radius = circle.getAttribute("r");
+        var circleLength = (radius * 2) * Math.PI;
+
+        percent.innerHTML = Math.ceil(100 * checkboxesChecked / checkboxesAll) + "%";
+        circle.style.strokeDashoffset = circleLength - (circleLength * checkboxesChecked / checkboxesAll);
+        circle.classList.add("_transition");
+    };
+
+    return {
+        init: _init
+    };
+})();
+
 window.onload = function() {
-    initCircle();
-    eventItem();
+    checkboxes && chartModule.init();
 };
-
-function eventItem() {
-    var item = document.querySelectorAll(".goal-checkbox");
-    
-    for (var i = 0; i < item.length; i++) {
-        item[i].addEventListener("click", initCircle);
-    }
-}
-
-function initCircle() {
-    var circle = document.querySelector(".goal-chart__active");
-    var checkboxesAll = document.querySelectorAll(".goal-checkbox").length;
-    var checkboxesChecked = document.querySelectorAll(".goal-checkbox:checked").length;
-    var percent = document.querySelector(".goal-chart__percent");
-    var radius = circle.getAttribute("r");
-    var circleLength = (radius * 2) * Math.PI;
-
-    percent.innerHTML = Math.ceil(100 * checkboxesChecked / checkboxesAll) + "%";
-    circle.style.strokeDashoffset = circleLength - (circleLength * checkboxesChecked / checkboxesAll);
-    circle.classList.add("_transition");
-}
