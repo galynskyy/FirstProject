fa33cb3d5da16dc3b12a26ddda2589e13857fb9e
sychuginaanna <sychuginaanna@ya.ru>
2017-01-20 13:07:20 +0300

    add task in left block on onload and onclick

---
+++ src/scripts/calendar.js
@@ -0,0 +1,54 @@
+(function(){
+	function TaskList(config){
+		var tasks = [];
+		var container_id = (config||{}).container_id||'tasks';
+		var container = document.getElementById(container_id);
+
+
+		this.add = function(task){
+			tasks.push(task);
+			this.draw(task);
+		};
+
+		this.redraw = function(){
+				container.innerHTML='';
+			for (var task in tasks){
+				this.draw(task);
+			}
+		};
+
+		this.draw = function(task){
+			var li = document.createElement('li');
+			var a = document.createElement('a');
+			var span = document.createElement('span');
+
+			li.classList.add('tasks-list__item');
+			a.classList.add('tasks-list__title');
+			span.classList.add('tasks-list__text');
+
+			span.innerText = task.name;
+			a.href="#";
+
+			a.appendChild(span);
+			li.appendChild(a);
+			container.appendChild(li);
+		}
+	}
+	var taskList = new TaskList();
+	taskList.add({name: 'Лендинг для корпоратива'});
+	taskList.add({name: 'Креатив на афишу'});
+	taskList.add({name: 'Отрисовка баннеров'});
+
+	//на onload
+	document.addEventListener('DOMContentLoaded', function(){
+		taskList.add({name: 'Новая задача'});
+	});
+
+	//на onclick
+	var btn = document.getElementById('add');
+	btn.onclick = function(){
+		taskList.add({name: 'Новая задача1'});
+	};
+
+}());
+
