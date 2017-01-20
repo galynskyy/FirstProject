a7f0b3a9eff2e18aa25f250e94c6b104166a1afa
sychuginaanna <sychuginaanna@ya.ru>
2017-01-20 21:03:55 +0300

    mistakes fix

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,23 +1,22 @@
-(function(){
-	function TaskList(config){
+(function() {
+	function TaskList(config) {
 		var tasks = [];
-		var container_id = (config||{}).container_id||'tasks';
+		var container_id = (config || {}).container_id || 'tasks';
 		var container = document.getElementById(container_id);
 
-
-		this.add = function(task){
+		this.add = function(task) {
 			tasks.push(task);
 			this.draw(task);
 		};
 
-		this.redraw = function(){
-				container.innerHTML='';
-			for (var task in tasks){
+		this.redraw = function() {
+			container.innerHTML='';
+			for (var task in tasks) {
 				this.draw(task);
 			}
 		};
 
-		this.draw = function(task){
+		this.draw = function(task) {
 			var li = document.createElement('li');
 			var a = document.createElement('a');
 			var span = document.createElement('span');
@@ -34,21 +33,20 @@
 			container.appendChild(li);
 		}
 	}
+
 	var taskList = new TaskList();
 	taskList.add({name: 'Лендинг для корпоратива'});
 	taskList.add({name: 'Креатив на афишу'});
 	taskList.add({name: 'Отрисовка баннеров'});
 
-	//на onload
-	document.addEventListener('DOMContentLoaded', function(){
+	document.addEventListener('DOMContentLoaded', function() {
 		taskList.add({name: 'Новая задача'});
 	});
 
-	//на onclick
 	var btn = document.getElementById('add');
-	btn.onclick = function(){
-		taskList.add({name: 'Новая задача1'});
-	};
 
+	btn.addEventListener( "click" , function() {
+		taskList.add({name: 'Новая задача1'});
+	});
 }());
 
