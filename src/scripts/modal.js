3570046c110dbda2743a7bf8d1abc4bd234f9263
andrewshevtsov <andrewshevtsov@inbox.ru>
2017-02-05 23:20:43 +0300

    add js to task-btn, add one input to modal window, remove js in html

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,8 +1,36 @@
-function showModal() {
-	var popup = document.getElementsByClassName("fixed-wrap")[0];
-	popup.classList.add("_show");
-}
+var btn = document.querySelector('.tasks__btn');
+console.log(btn);
 
-function closeModal() {
-	
-}
+btn.addEventListener('click', function(){
+	var modal = document.querySelector('.fixed-wrap');
+	console.log(modal);
+	modal.classList.toggle("_show");
+});
+
+
+/*
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".tasks__btn").addEventListener("click", function() {
+        var popup = document.querySelector(".fixed-wrap");
+        	popup.classList.toggle("_show");
+    })
+});
+*/
+
+/*document.addEventListener("DOMContentLoaded", function() {
+	var btnPopup = document.getElementsByClassName("tasks__btn")[0].addEventListener("click", function() {
+		var popup =	document.getElementsByClassName("fixed-wrap")[0];
+		window.setTimeout(function() {
+            popup.classList.add("_show")
+        }, 0)
+		popup.classList.add("_show");
+	})
+});*/
+
+/*
+function() {
+	var popup = document.getElementsByClassName("tasks__btn")[0].addEventListener("click", function() {
+		document.getElementsByClassName("fixed-wrap")[0].classList.add("_show");
+	});
+};
+*/
