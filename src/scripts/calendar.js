cfd48108127e8205fe6bcacac112dea7ab64dd2d
sychuginaanna <sychuginaanna@ya.ru>
2017-03-19 22:56:35 +0300

    codestyle

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,144 +1,141 @@
 var btn = document.getElementById("add");
 
-var CalendarModule = (function () {
-    var _init = function (config) {
-        _eventListener();
-        if (config && config.tasks) {
-            config.tasks.forEach(_draw);
-        }
-    };
+var CalendarModule = (function() {
+	var _init = function(config) {
+		_eventListener();
+		if (config && config.tasks) {
+			config.tasks.forEach(_draw);
+		}
+	};
 
-    var _eventListener = function() {
-        btn.addEventListener("click", _addOnCLick);
-    };
+	var _eventListener = function() {
+		btn.addEventListener("click", _addOnCLick);
+	};
 
-    var _addOnCLick = function addOnCLick() {
+	var _addOnCLick = function addOnCLick() {
+		var taskName = document.getElementById("input-form").value;
 
+		if (taskName.length === 0 || _checkIfTaskAlreadyExists(taskName)) {
+			alert("Задача с таким названием уже существует");
+			return;
+		}
 
-        var taskName = document.getElementById("input-form").value;
+		var d = $('#datetimepicker').datetimepicker("getValue");
 
-        if (taskName.length === 0 || _checkIfTaskAlreadyExists(taskName)) {
-            alert("Задача с таким названием уже существует");
-            return;
-        }
+		_draw({
+			name: taskName,
+			enddate: d
+		});
+	};
 
-        var d = $('#datetimepicker').datetimepicker("getValue");
+	var _checkIfTaskAlreadyExists = function(taskName) {
+		var task_container = document.getElementById("tasks");
+		var taskElements = task_container.querySelectorAll('.tasks-list__text');
+		var namesList = Array.prototype.map.call(taskElements, function(element) {
 
-        _draw({
-            name: taskName,
-            enddate: d
-        });
-    };
+			return element.textContent;
+		});
 
-    var _checkIfTaskAlreadyExists = function(taskName) {
-        var task_container = document.getElementById("tasks");
-        var taskElements = task_container.querySelectorAll('.tasks-list__text');
-        var namesList = Array.prototype.map.call(taskElements, function(element) {
+		return namesList.indexOf(taskName) > -1;
+	};
 
-            return element.textContent;
-        });
+	var _draw = function(task) {
+		var container_id = "tasks";
+		var task_calendar_id = "task-table";
+		var task_container = document.getElementById(container_id);
+		var calendar_container = document.getElementById(task_calendar_id);
+		var tr = document.createElement("tr");
 
-        return namesList.indexOf(taskName) > -1;
-    };
-
-    var _draw = function(task) {
-        var container_id = "tasks";
-        var task_calendar_id = "task-table";
-        var task_container = document.getElementById(container_id);
-        var calendar_container = document.getElementById(task_calendar_id);
-        var tr = document.createElement("tr");
-
-        tr.className = "calendar__columns";
-        calendar_container.appendChild(tr);
-        task_container.appendChild(_getTask(task));
-        _getCalendar(tr, task);
-    };
+		tr.className = "calendar__columns";
+		calendar_container.appendChild(tr);
+		task_container.appendChild(_getTask(task));
+		_getCalendar(tr, task);
+	};
 
 
-    var _getTask = function (task) {
-        var li = document.createElement("li");
-        var a = document.createElement("a");
-        var span = document.createElement("span");
-        var button = document.createElement("a");
-        var span1 = document.createElement("span");
-        var span2 = document.createElement("span");
+	var _getTask = function(task) {
+		var li = document.createElement("li");
+		var a = document.createElement("a");
+		var span = document.createElement("span");
+		var button = document.createElement("a");
+		var span1 = document.createElement("span");
+		var span2 = document.createElement("span");
 
-        li.classList.add("tasks-list__item");
-        a.classList.add("tasks-list__title");
-        span.classList.add("tasks-list__text");
-        span.innerText = task.name;
-        a.href = "#";
-        button.href = "#";
+		li.classList.add("tasks-list__item");
+		a.classList.add("tasks-list__title");
+		span.classList.add("tasks-list__text");
+		span.innerText = task.name;
+		a.href = "#";
+		button.href = "#";
 
-        button.classList.add("tasks-list__close");
-        a.appendChild(span);
-        button.appendChild(span1);
-        button.appendChild(span2);
+		button.classList.add("tasks-list__close");
+		a.appendChild(span);
+		button.appendChild(span1);
+		button.appendChild(span2);
 
-        li.appendChild(a);
-        a.appendChild(button);
+		li.appendChild(a);
+		a.appendChild(button);
 
-        return li;
-    };
+		return li;
+	};
 
-    var _getCalendar = function (tr, task) {
+	var _getCalendar = function(tr, task) {
+		var td = document.createElement("td");
+		var divInner = document.createElement("div");
+		var div = document.createElement("div");
+		var span = document.createElement("span");
+		var now = moment();
+		var endDate = moment(task.enddate);
+		var startDate = moment(task.startdate);
+		var daysStart = moment({
+			year: now.year(),
+			month: now.month(),
+			days: 1
+		}).diff(startDate, "days");
 
-        var td = document.createElement("td");
-        var divInner = document.createElement("div");
-        var div = document.createElement("div");
-        var span = document.createElement("span");
-        var now = moment();
-        var endDate = moment(task.enddate);
-        var startDate = moment(task.startdate);
-        var daysStart = moment({
-            year: now.year(),
-            month: now.month(),
-            days: 1
-        }).diff(startDate, "days");
+		var daysEnd = endDate.diff(startDate.add(-1, "days"), "days");
 
-        var daysEnd = endDate.diff(startDate.add(-1, "days"), "days");
+		if (daysStart < 0) {
+			var td2 = document.createElement("td");
+			tr.appendChild(td2);
+			td2.colSpan = Math.abs(daysStart);
+		}
 
-        if (daysStart < 0) {
-            var td2 = document.createElement("td");
-            tr.appendChild(td2);
-            td2.colSpan = Math.abs(daysStart);
-        }
+		td.className = "calendar__day";
+		td.colSpan = daysEnd;
+		divInner.className = "calendar__inner";
+		div.className = "calendar__task-line";
+		span.className = "calendar__progress";
+		if (task.isDone) {
+			span.className += " _delay";
+		} else {
+			span.className += " _done";
+		}
 
-        td.className = "calendar__day";
-        td.colSpan = daysEnd;
-        divInner.className = "calendar__inner";
-        div.className = "calendar__task-line";
-        span.className = "calendar__progress";
-        if (task.isDone) {
-            span.className += " _delay";
-        } else {
-            span.className += " _done";
-        }
+		div.appendChild(span);
+		divInner.appendChild(div);
+		td.appendChild(divInner);
+		tr.appendChild(td);
+	};
 
-        div.appendChild(span);
-        divInner.appendChild(div);
-        td.appendChild(divInner);
-        tr.appendChild(td);
-    };
-
-    return {
-        init: _init
-    }
+	return {
+		init: _init
+	}
 })();
 
 btn && CalendarModule.init({
-    tasks: [
-        {
-            name: "Лендинг для корпоратива",
-            enddate: moment("2017-03-28").toString(),
-            progress: 2,
-            startdate: moment("2017-03-11").toString()
-        },
-        {
-            name: "Креатив на афишу",
-            enddate: moment("2017-03-27").toString(),
-            isDone: true,
-            startdate: moment("2017-03-10").toString()
-        }
-    ]
+	tasks: [
+		{
+			name: "Лендинг для корпоратива",
+			enddate: moment("2017-03-28").toString(),
+			progress: 2,
+			startdate: moment("2017-03-11").toString()
+		},
+		{
+			name: "Креатив на афишу",
+			enddate: moment("2017-03-27").toString(),
+			isDone: true,
+			startdate: moment("2017-03-10").toString()
+		}
+	]
 });
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,16 +1,16 @@
 var btn = document.querySelector('.tasks__btn');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
 	var modal = document.querySelector('.fixed-wrap');
 	modal.classList.toggle("_show");
-    var elem = document.getElementsByClassName('fixed-wrap fixed-wrap__modal _show')[0];
-    elem.style.display="block";
+	var elem = document.getElementsByClassName('fixed-wrap fixed-wrap__modal _show')[0];
+	elem.style.display = "block";
 });
 
 
 var closeBtn = document.getElementById('close');
 
-closeBtn.addEventListener('click', function(){
-    var elem = document.getElementsByClassName('fixed-wrap fixed-wrap__modal _show')[0];
-    elem.style.display="none";
+closeBtn.addEventListener('click', function() {
+	var elem = document.getElementsByClassName('fixed-wrap fixed-wrap__modal _show')[0];
+	elem.style.display = "none";
 });
