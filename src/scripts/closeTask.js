05e6610786ad7c0c3478d53e0a68f7241f45862b
sychuginaanna <sychuginaanna@ya.ru>
2017-03-20 17:10:33 +0300

    Удаление задачи из списка

--- src/scripts/closeTask.js
+++ src/scripts/closeTask.js
@@ -1,8 +1,26 @@
-// var btnClose = document.getElementsByClassName("tasks-list__close")[0];
-//
-//
-// function onBtnClick() {
-//
-// }
-//
-// btnClose.addEventListener("click", onBtnClick);
+
+
+var taskContainer = document.getElementById("tasks");
+
+function onListOfTasksClick(e) {
+	var target =  e.target;
+
+	if (isCloseBtn(target)) {
+		var taskLi = target.parentNode.parentNode;
+		deleteTask(taskLi);
+	}
+
+}
+
+
+function deleteTask(element) {
+	taskContainer.removeChild(element);
+}
+
+function isCloseBtn(target) {
+	return target.classList.contains("tasks-list__close");
+}
+
+
+taskContainer.addEventListener('click', onListOfTasksClick);
+
