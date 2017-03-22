7fa1255b282f1f60ae5f72fae317e8376b078098
sychuginaanna <sychuginaanna@ya.ru>
2017-03-22 13:41:37 +0300

    validator patern module

--- src/scripts/validator.js
+++ src/scripts/validator.js
@@ -3,47 +3,69 @@ var modalError = document.getElementById('error');
 var minLength = 4;
 var maxLength = 20;
 
-var _onInputBlur = function() {
-	if (this.value.length === 0) {
-		_colorInputBorder(inputElement, "red");
-		return;
-	}
+var validateModule =  (function() {
+	var _init = function() {
+		_eventListener();
+	};
 
-	if (_checkIfTaskAlreadyExists(this.value)) {
-		_colorInputBorder(this, "red");
-		return;
-	}
-
-	if (this.value.length < minLength) {
-		modalError.textContent = "Введите не менее 4 символов";
-		_colorInputBorder(this, "red");
-		return;
-	}
-
-	if (this.value.length > maxLength) {
-		modalError.textContent = "Введите не более 20 символов";
-		_colorInputBorder(this, "red");
-		return;
-	}
-
-	_colorInputBorder(this, "#acadaf");
-};
-
-inputElement.addEventListener('blur', _onInputBlur);
-
-var _colorInputBorder = function(input, color) {
-	input.style.border = `1px solid ${color}`;
-};
+	var _eventListener = function() {
+		inputElement.addEventListener('blur', _onInputBlur);
+	};
 
 
-var _checkIfTaskAlreadyExists = function(taskName) {
-	var taskContainer = document.getElementById("tasks");
-	var taskElements = taskContainer.querySelectorAll('.tasks-list__text');
-	var namesList = Array.prototype.map.call(taskElements, function(element) {
+	var _onInputBlur = function() {
+		if (this.value.length === 0) {
+			_colorInputBorder(inputElement, "red");
+			return;
+		}
 
-		return element.textContent;
-	});
+		if (_checkIfTaskAlreadyExists(this.value)) {
+			_colorInputBorder(this, "red");
+			return;
+		}
 
-	return namesList.indexOf(taskName) > -1;
-};
+		if (this.value.length < minLength) {
+			modalError.textContent = "Введите не менее 4 символов";
+			_colorInputBorder(this, "red");
+			return;
+		}
+
+		if (this.value.length > maxLength) {
+			modalError.textContent = "Введите не более 20 символов";
+			_colorInputBorder(this, "red");
+			return;
+		}
+
+		_colorInputBorder(this, "#acadaf");
+	};
+
+	var _colorInputBorder = function(input, color) {
+		input.style.border = `1px solid ${color}`;
+	};
+
+	var _checkIfTaskAlreadyExists = function(taskName) {
+		var taskContainer = document.getElementById("tasks");
+		var taskElements = taskContainer.querySelectorAll('.tasks-list__text');
+		var namesList = Array.prototype.map.call(taskElements, function(element) {
+
+			return element.textContent;
+		});
+
+		return namesList.indexOf(taskName) > -1;
+	};
+
+	return {
+		init: _init
+	};
+})();
+inputElement && validateModule.init();
+
+
+
+
+
+
+
+
+
 
